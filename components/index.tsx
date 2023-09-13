04daa36b47fcd589dc3eb02ae518d7393ebb110a
Amelia LI <jkramelia8872@gmail.com>
2023-09-13 15:10:58 -0400

    add search bar make and model

--- components/index.tsx
+++ components/index.tsx
@@ -2,10 +2,14 @@ import Hero from "./Hero";
 import CustomButton from "./CustomButton";
 import NavBar from "./NavBar";
 import Footer from "./Footer";
+import SearchBar from "./SearchBar";
+import SearchManufacturer from "./SearchManufacturer";
 
 export {
   Hero,
   CustomButton,
   NavBar,
-  Footer
+  Footer,
+  SearchManufacturer,
+  SearchBar
 };
